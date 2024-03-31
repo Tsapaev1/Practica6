c97356604565ee8260f3b0a3532ece96d8572ccd
Tsapaev1 <is25tsapaevaa@artcollege.ru>
2024-03-31 15:23:57 +0300

    Рассчёт индвидуального

--- pr6/Program.cs
+++ pr6/Program.cs
@@ -14,7 +14,8 @@ namespace pr6
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
             double F = (Math.Sin(x) + Math.Cos(x)) / 2;
-            Console.WriteLine($"Для значения х = {x}, F = {F}.");
+            double G = (8*Math.Sin(x) - Math.Cos(2*x));
+            Console.WriteLine($"Для значения х = {x}, G = {G}.");
             Console.ReadLine();
         }
     }
